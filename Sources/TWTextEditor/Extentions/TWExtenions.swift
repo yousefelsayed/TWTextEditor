09ae10de11b66d703051226e541c5ebb9de280ce
Yousef Elsayed <yousefe.elsayed@gmail.com>
2022-10-04 20:43:06 +0200

    Calculate Characters of different types of input to TWTextEditor

--- Sources/TWTextEditor/Extentions/TWExtenions.swift
+++ Sources/TWTextEditor/Extentions/TWExtenions.swift
@@ -240,3 +240,30 @@ extension UIView {
         layer.mask = mask
     }
 }
+
+
+extension Character {
+    /// A simple emoji is one scalar and presented to the user as an Emoji
+    var isSimpleEmoji: Bool {
+        guard let firstScalar = unicodeScalars.first else { return false }
+        return firstScalar.properties.isEmoji && firstScalar.value > 0x238C
+    }
+
+    /// Checks if the scalars will be merged into an emoji
+    var isCombinedIntoEmoji: Bool { unicodeScalars.count > 1 && unicodeScalars.first?.properties.isEmoji ?? false }
+
+    var isEmoji: Bool { isSimpleEmoji || isCombinedIntoEmoji }
+}
+
+extension UIViewController {
+    
+    @objc public func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
--- Sources/TWTextEditor/Views/TWTextEditorView.swift
+++ Sources/TWTextEditor/Views/TWTextEditorView.swift
@@ -19,9 +19,14 @@ public class TWTextEditorView: UIView {
     
     public var placeholderLabel : UILabel!
     
+    public var typedCharachtersCount: Int! { return TWTextEditorView.lengthOf(tweet: self.text) }
     
-    public  var twTextColor: UIColor?
-    var twTextFont: UIFont?
+    public var remainingCharachtersCount: Int! { return (self.maxCountOfCharchters - typedCharachtersCount) }
+    
+    public var maxCountOfCharchters: Int = 280
+    
+    private var text: String { return self.textView.text }
+    
     weak public  var viewDelegate: TWTextEditorViewDelegate?
     
     required init?(coder aDecoder: NSCoder) {
@@ -43,8 +48,8 @@ public class TWTextEditorView: UIView {
     }
     
     //MARK:- Setup text view UI
-  private func setupTextEditor() {
-        self.textView.textColor = self.twTextColor
+    private func setupTextEditor() {
+        self.textView.textColor = .black
         self.textView.font = .twFontReqularWithSize(size: 14)
         self.view.layer.borderWidth = 1.0
         self.view.layer.cornerRadius = 12
@@ -64,14 +69,82 @@ public class TWTextEditorView: UIView {
         self.placeholderLabel.textColor = UIColor.twPlaceHolderColor
         self.placeholderLabel.alpha = (textView.text.isEmpty) ? 1 : 0
     }
+    
+    //MARK:- Dimiss keyboard when user click outside the textview 
+    public func dimissKeyboard() {
+        self.textView.resignFirstResponder()
+    }
+    
+    //MARK:- Clear all the current text
+   public func clearText() {
+        self.textView.text = ""
+        self.textViewDidChange(self.textView)
+    }
+    
+    //MARK:- Copy the current text to clipboard
+    public func copyCurrentText() {
+        UIPasteboard.general.string = self.text
+    }
+    
+    //MARK:- Calculate length of given tweet
+    static public func lengthOf(tweet: String) -> Int {
+        
+        let (cleanText, urlLength) = cleanAndCountURLs(text: tweet)
+        
+        var weightedLength = urlLength
+        
+        for char in cleanText {
+            if char.isEmoji == true {
+                weightedLength += 2 //2 is the count of characters for any emoji according to twitter docs
+                continue
+            } else {
+                weightedLength += 1
+            }
+        }
+        
+        return weightedLength
+    }
+    
+    static public  func cleanAndCountURLs(text: String) -> (String, Int) {
+        var output = text
+        var lenght = 0
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        let matches = detector.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count))
+        
+        matches.forEach {
+            output = (text as NSString).replacingCharacters(in: $0.range, with: "")
+            lenght += 23 //23 is the count of characters for any url according to twitter docs
+        }
+        return (output, lenght)
+    }
+    
+    
 }
 
 extension TWTextEditorView: UITextViewDelegate {
+    
     public func textViewDidChange(_ textView: UITextView) {
-        DispatchQueue.main.async {
-            self.placeholderLabel.alpha = (textView.text.isEmpty) ? 1 : 0
-            self.viewDelegate?.textDidChange(textView)
+        DispatchQueue.main.async { [weak self] in
+            
+            self?.placeholderLabel.alpha = (textView.text.isEmpty) ? 1 : 0
+            
+            self?.viewDelegate?.textDidChange(textView)
         }
+    }
+    
+    public func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        // get the current text, or use an empty string if that failed
+          let currentText = textView.text ?? ""
 
+          // attempt to read the range they are trying to change, or exit if we can't
+          guard let stringRange = Range(range, in: currentText) else { return false }
+
+          // add their new text to the existing text
+          let updatedText = currentText.replacingCharacters(in: stringRange, with: text)
+
+        // make sure the result is under 280 characters
+           return updatedText.count <= maxCountOfCharchters
+        
+    
     }
 }
